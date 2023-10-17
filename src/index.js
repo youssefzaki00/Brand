fc819e1cbd9884b2f8bf0d0cdd7c2ebe50ed524e
youssefzaki00 <107646038+youssefzaki00@users.noreply.github.com>
2023-10-17 10:16:19 +0300

    vercel analytics install

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ import { PriceRangeProvider } from './Components/Filters/PriceContext';
 import { ProductsProvider } from './ProductsContext';
 import {  SavedProductsProvider } from './SavedContext';
 import { CartProductsProvider } from './CartContext';
+import { Analytics } from "@vercel/analytics/react";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -26,6 +28,7 @@ root.render(
                   <CartProductsProvider>
                     <App />
                     <ToastContainer />  
+                    <Analytics/>
                   </CartProductsProvider>
                 </SavedProductsProvider>
               </ProductsProvider>
