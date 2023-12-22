2e2406cf28c764711c549562ee48401601b680a0
youssefzaki00 <107646038+youssefzaki00@users.noreply.github.com>
2023-12-22 09:04:21 +0200

    v 0.2.6

--- src/Components/Tags.js
+++ src/Components/Tags.js
@@ -1,36 +1,33 @@
+import React, { useEffect, useState } from "react";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-// import { useProductContext } from '../ProductChosen';
 import { Link, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function Tags() {
-  // const { chosenProduct,setChosenProduct } = useProductContext();
   const { category, name } = useParams();
+  let location = useLocation();
+  const [pathName, setPathName] = useState(["Home"]); // initialize with "Home"
+
+  useEffect(() => {
+    setPathName(location.pathname.split("/").filter(Boolean));
+  }, [location]);
+
   return (
-    <div className="tags flex  gap-2 items-center mb-4 text-xs">
-      <Link to="/" className="hover:text-blue-800 text-blue-500">
+    <div className="tags flex gap-2 items-center mb-4 text-sm">
+      <Link to="/" className="hover:text-blue-800 text-blue-600">
         Home
       </Link>
-      <FontAwesomeIcon icon={faAngleRight} className="" />
-      <Link
-        to="/AllCategory/AllProducts"
-        className="hover:text-red-800 text-red-500"
-      >
-        All Products
-      </Link>
-      {category && (
-        <>
-          <FontAwesomeIcon icon={faAngleRight} />
-          <button className="hover:text-green-800 text-green-500 cursor-pointer">
-            {category}
-          </button>
-          <FontAwesomeIcon icon={faAngleRight} className="" />
-          <button className="hover:text-yellow-800 text-yellow-500 cursor-pointer">
-            {name}
-          </button>
-        </>
-      )}
+      {pathName.map((path, index) => (
+        <React.Fragment key={index}>
+          <>
+            <FontAwesomeIcon icon={faAngleRight} className="" />
+            <button className="hover:text-red-800 text-red-600 cursor-pointer">
+              {path}
+            </button>
+          </>
+        </React.Fragment>
+      ))}
     </div>
   );
 }
